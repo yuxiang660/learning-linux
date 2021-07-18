34af51e12b296770399e499ad8c9ca0d4dcb1f1c
yuxiangw <yuxian660@163.com>
2021-07-18 15:30:02 +0800

    clean code

--- the_probability_lifesaver/code/birth/same_birth_prob.py
+++ the_probability_lifesaver/code/birth/same_birth_prob.py
@@ -5,14 +5,13 @@ import matplotlib.pyplot as plt
 
 num_of_persons = range(1, 50)
 NUM_DAYS_PER_YEAR = 365
-current_no_same_birth_prob = 1
+no_same_birth_prob = 1
 
 # one point: (number of persons, probability)
 at_least_two_same_birth_probs = []
 for n in num_of_persons:
-    no_same_birth_prob = (NUM_DAYS_PER_YEAR - (n - 1)) / NUM_DAYS_PER_YEAR
-    current_no_same_birth_prob = current_no_same_birth_prob * no_same_birth_prob
-    at_least_two_same_birth_probs.append(1 - current_no_same_birth_prob)
+    no_same_birth_prob = ((NUM_DAYS_PER_YEAR - (n - 1)) / NUM_DAYS_PER_YEAR) * no_same_birth_prob
+    at_least_two_same_birth_probs.append(1 - no_same_birth_prob)
 
 plt.plot(num_of_persons, at_least_two_same_birth_probs, label='birth_prob')
 plt.xlabel('Number of Persons')
