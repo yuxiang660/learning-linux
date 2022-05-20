cac73930370f94efc483912a04374c53bf11a94e
yuxiangw <yuxian660@163.com>
2022-05-20 17:10:11 +0800

    add test

--- concurrency_with_modern_c++/code/stl/par_unseq_atomic/main.cpp
+++ concurrency_with_modern_c++/code/stl/par_unseq_atomic/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <mutex>
+#include <atomic>
 
 std::mutex m;
 
@@ -12,11 +13,12 @@ int main()
 {
    std::vector<int> v{1, 2, 3, 4, 5};
 
-   int sum = 0;
-   std::for_each(std::execution::par, v.begin(), v.end(), [&sum](int i)
+   // 必须是原子类型，不然`par_unseq`会产生死锁，因为可能同一线程连续两次调用`m.lock`，后一次锁一直等待前一次释放锁
+   std::atomic<int> sum = 0;
+   std::for_each(std::execution::par_unseq, v.begin(), v.end(), [&sum](int i)
                  {
                      std::lock_guard<std::mutex> lock(m);
-                     sum += i; });
+                     sum += i + i; });
 
    std::cout << "sum = " << sum << std::endl;
    return 0;
