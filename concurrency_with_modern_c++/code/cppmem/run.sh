124ce82ef4272f7af6e38d7c37765388ba5328f9
yuxiangw <yuxian660@163.com>
2022-05-20 20:26:23 +0800

    update test

--- concurrency_with_modern_c++/code/cppmem/run.sh
+++ concurrency_with_modern_c++/code/cppmem/run.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/bash
 
 n=4
-
 if [ ! -z "$1" ]; then
    n=$1
+fi
+
+y=1
+if [ ! -z "$2" ]; then
+   y=$2
+fi
+
+x=1
+if [ ! -z "$3" ]; then
+   x=$3
 fi
 
 for ((i = 1; i <= $n; i++)); do
    echo "--- Test $i ---" > run.log
    ./main >> run.log
-   y_1=`grep -e 'y = 1' run.log | wc -l`
-   x_0=`grep -e 'x = 0' run.log | wc -l`
-   if [[ $y_1 == 1 && $x_0 == 1 ]]; then
-      echo "Find 'y=1,x=0' after $i run";
+   y_match=`grep -e "y = $y" run.log | wc -l`
+   x_match=`grep -e "x = $x" run.log | wc -l`
+   if [[ $y_match == 1 && $x_match == 1 ]]; then
+      echo "Find 'y=$y,x=$x' after $i run";
       cat run.log;
       exit;
    fi
 done
 
-echo "No 'y=1,x=0' after $n run"
+echo "No 'y=$y,x=$x' after $n run"
