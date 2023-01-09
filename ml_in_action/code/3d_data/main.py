f732752ec1e5768ab8243e47516ac7ed5535321d
yuxiangw <yuxiangw@cadence.com>
2023-01-08 23:35:49 -0800

    update

--- ml_in_action/code/3d_data/main.py
+++ ml_in_action/code/3d_data/main.py
@@ -11,20 +11,28 @@ logging.basicConfig(level=logging.INFO,
 data = {
     "X": [1, 2, 3, 2.5, 4, 5],
     "Y": [1, 1, 2, 2, 3, 6],
-    "Z": [3, 4, 7, 6.5, 10, 17],
+    "Z": [3, 4, 7, 6.5, 10, 20],
 }
+
 
 def main():
     df = pd.DataFrame(data)
-    x = np.array(df["X"])
-    y = np.array(df["Y"])
-    z = np.array(df["Z"])
-    X,Y = np.meshgrid(x, y)
-    fig1 = go.Figure(data=[
+    fig1 = px.scatter_3d(df, x='X', y='Y', z='Z')
+
+    x = np.arange(0, 10, 1)
+    y = np.arange(0, 10, 1)
+    X, Y = np.meshgrid(x, y)
+    fig2 = go.Figure(data=[
         go.Surface(x=x, y=y, z=X+2*Y),
     ])
-    fig2 = px.scatter_3d(df, x='X', y='Y', z='Z')
+
     fig3 = go.Figure(data=fig1.data + fig2.data)
+    fig3.update_layout(title='Linear',
+                       scene=dict(
+                           xaxis=dict(range=[-5, 30],),
+                           yaxis=dict(range=[-5, 30],),
+                           zaxis=dict(range=[-5, 30],),),
+                       margin=dict(l=50, r=50, b=100, t=50))
     fig3.write_html("./data.html")
 
 
